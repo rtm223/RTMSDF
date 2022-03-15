43f3bb7d208cb9bd971740f38a8b54a3944f3836
Richard Meredith <rtm223@hotmail.com>
2022-03-15 12:07:02 +0100

    Moved config file location, corrected displaynames for suffix settings

--- Source/RTMSDFEditor/Private/Config/RTMSDFConfig.h
+++ Source/RTMSDFEditor/Private/Config/RTMSDFConfig.h
@@ -8,13 +8,13 @@
 #include "UObject/Object.h"
 #include "RTMSDFConfig.generated.h"
 
-UCLASS(Config=Game, DefaultConfig)
+UCLASS(Config=RTMSDF, DefaultConfig)
 class RTMSDFEDITOR_API URTMSDFConfig : public UObject
 {
 	GENERATED_BODY()
 
 public:
-	UPROPERTY(EditAnywhere, Category="SVG File Rules", Config, meta=(DisplayName="SVG Filename Prefix"))
+	UPROPERTY(EditAnywhere, Category="SVG File Rules", Config, meta=(DisplayName="SVG Filename Suffix"))
 	FString SVGFilenameSuffix = "_SDF";
 
 	UPROPERTY(EditAnywhere, Category="SVG Default Import Settings", Config)
@@ -23,7 +23,7 @@ public:
 	UPROPERTY(EditAnywhere, Category="SVG Default Import Settings", Config, meta=(FullyExpand=true, DisplayName = "Default SVG Import Settings"))
 	FRTMSDF_SVGImportSettings DefaultSVGImportSettings;
 
-	UPROPERTY(EditAnywhere, Category="Bitmap File Rules", Config, meta=(DisplayName="Bitmap Filename Prefix"))
+	UPROPERTY(EditAnywhere, Category="Bitmap File Rules", Config, meta=(DisplayName="Bitmap Filename Suffix"))
 	FString BitmapFilenameSuffix = "_SDF";
 
 	UPROPERTY(EditAnywhere, Category="Bitmap Default Import Settings", Config, meta=(FullyExpand=true, DisplayName = "Default SVG Import Settings"))
